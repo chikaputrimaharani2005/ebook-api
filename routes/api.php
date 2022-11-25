13017ee8dcf9d19fb66b76109f6317dce431fcd5
Your Name <you@example.com>
2022-11-25 09:30:59 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,18 @@ Route::post('siswa/{id}',[SiswaController::class, 'update']);
 Route::delete('siswa/{id}', [SiswaController::class, 'destroy']);
 
 Route :: resource ('siswa',Siswacontroller::class);
+ //public route
+ Route::post('/register',[AuthController::class,'register']);
+ Route::post('/login',[AuthController::class,'login']);
+ 
+ Route::get('/books',[BookController::class,'index']);
+ Route::get('/Books/{id}',[BooksController::class,'show']);
+ Route::get('/Authors',[AuthorController::class,'index']);
+ Route::get('/Authors/{id}',[AuthorController::class,'show']);
+
+ //protected routes
+ Route::middleware('auth:sanctum')->group(function(){
+      Route::resource('/books', BookController::class)->except('create','edit','show','index');
+      Route::post('/logout',[AuthController::class,'logout']);
+      Route::resource('authors', AuthorController::class)->except('create','edit','show','index');
+});
